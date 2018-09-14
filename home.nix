601a0e8676b7829b8ed46d17d131b8c9b5b357af
William Hua <william@attente.ca>
2018-09-13 23:29:34 -0400

    enable git

--- home.nix
+++ home.nix
@@ -5,4 +5,8 @@
   programs.home-manager.path = "/home/william/.home-manager";
 
   programs.termite.enable = true;
+
+  programs.git.enable = true;
+  programs.git.userName = "William Hua";
+  programs.git.userEmail = "william@attente.ca";
 }
