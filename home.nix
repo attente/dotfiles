e3db1389629925adede532881e59be5c5f2424a8
William Hua <william@attente.ca>
2019-04-14 00:03:31 -0400

    show line numbers

--- home.nix
+++ home.nix
@@ -37,6 +37,7 @@ let mozilla = import (builtins.fetchGit {
   programs.neovim.vimAlias = true;
   programs.neovim.configure = {
     customRC = ''
+      set nu
       set et
       set si
       set sts=2
