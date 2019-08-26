34120be761c20b5b7f5d68dbc15fa14a0329b446
William Hua <william@attente.ca>
2019-08-26 09:20:25 -0400

    force ssh clone for private repo
    https://golang.org/doc/faq#git_https

--- home.nix
+++ home.nix
@@ -64,6 +64,13 @@ let mozilla = import (builtins.fetchGit {
   programs.git.enable = true;
   programs.git.userName = "William Hua";
   programs.git.userEmail = "william@attente.ca";
+  programs.git.extraConfig = {
+    url = {
+      "ssh://git@github.com/horizon-games" = {
+        insteadOf = "https://github.com/horizon-games";
+      };
+    };
+  };
 
   home.packages = with pkgs; [
     binaryen
