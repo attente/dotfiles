698f287554755c2fb17410869b5badd9e336680d
William Hua <william@attente.ca>
2019-11-05 12:26:22 -0500

    move secrets.nix

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-let secrets = import /home/william/.william/etc/secrets.nix; in
+let secrets = import /home/william/.william/secrets.helium.nix; in
 
 { config, pkgs, ... }:
 
