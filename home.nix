5380b45c7f74be202d6edbbd137c04e66bfa068b
William Hua <william@attente.ca>
2021-01-15 17:34:00 -0500

    bump rust nightly to 2021-01-15

--- home.nix
+++ home.nix
@@ -16,7 +16,7 @@ let mozilla = import (builtins.fetchGit {
 
         rustChannels.nightly.rust = (super.rustChannelOf {
           channel = "nightly";
-          date = "2021-01-01";
+          date = "2021-01-15";
         }).rust.override {
           targets = [
             "wasm32-unknown-unknown"
