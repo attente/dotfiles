36317de2c5053f9a70d4ef3279ab29255b6ec094
William Hua <william@attente.ca>
2021-11-01 23:45:40 -0400

    bind K to show_documentation

--- home.nix
+++ home.nix
@@ -66,6 +66,18 @@ let mozilla = import (builtins.fetchGit {
     nmap <silent> gy <Plug>(coc-type-definition)
     nmap <silent> gi <Plug>(coc-implementation)
     nmap <silent> gr <Plug>(coc-references)
+    nmap <leader>rn <Plug>(coc-rename)
+    nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+    function! s:show_documentation()
+      if (index(['vim','help'], &filetype) >= 0)
+        execute 'h '.expand('<cword>')
+      elseif (coc#rpc#ready())
+        call CocActionAsync('doHover')
+      else
+        execute '!' . &keywordprg . " " . expand('<cword>')
+      endif
+    endfunction
   '';
 
   programs.git.enable = true;
