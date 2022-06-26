342d715af214169b02eeae9c4ffc0127f835f954
William Hua <william@attente.ca>
2022-06-26 14:47:19 -0400

    kernel modules and params

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,17 @@ let secrets = import /etc/nixos/secrets.nix; in
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  boot.initrd.kernelModules = [
+    "amdgpu"
+    "lm92"
+    "nct6775"
+  ];
+
+  boot.kernelParams = [
+    "video=DP-1:1920x1080@60"
+    "video=DP-2:1920x1080@60"
+  ];
+
   boot.kernel.sysctl = {
     "fs.inotify.max_user_watches" = 65536;
   };
