a4f628c6965b8299ae52bb5ba39e7eeb6efe6acc
William Hua <william@attente.ca>
2022-07-04 10:06:44 -0400

    bump go to 1.18

--- configuration.nix
+++ configuration.nix
@@ -537,7 +537,7 @@ let secrets = import /etc/nixos/secrets.nix; in
       gnome3.nautilus
       gnumake
       gnupg
-      go
+      go_1_18
       google-cloud-sdk
       imagemagick
       inkscape
