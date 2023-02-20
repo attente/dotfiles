f7026f7444dbf4bda32f8fad2888a0d9d832e510
William Hua <william@attente.ca>
2023-02-20 16:25:55 -0500

    install gke-gcloud-auth-plugin

--- configuration.nix
+++ configuration.nix
@@ -602,7 +602,7 @@ let secrets = import /etc/nixos/secrets.nix; in
       gnumake
       gnupg
       go_1_18
-      google-cloud-sdk
+      (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
       imagemagick
       inkscape
       inotify-tools
