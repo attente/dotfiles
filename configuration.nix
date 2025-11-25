e5c305fff21bc8c055153a1db850b3e14c2f560f
William Hua <william@attente.ca>
2025-11-25 08:54:20 -0500

    install trezord

--- configuration.nix
+++ configuration.nix
@@ -627,6 +627,8 @@ let secrets = import /etc/nixos/secrets.nix; in
     libvirtd.enable = true;
   };
 
+  services.trezord.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
