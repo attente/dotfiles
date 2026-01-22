2f7fc3ed2d1d9898e5c42d86699d6eec960ce33e
William Hua <william@attente.ca>
2026-01-22 01:23:59 -0500

    disable adb

--- configuration.nix
+++ configuration.nix
@@ -114,7 +114,6 @@ let secrets = import /etc/nixos/secrets.nix; in
     description = "William Hua";
     hashedPassword = secrets.users.william.hashedPassword;
     extraGroups = [
-      "adbusers"
       "docker"
       "kvm"
       "libvirtd"
@@ -383,8 +382,6 @@ let secrets = import /etc/nixos/secrets.nix; in
   programs.ssh.startAgent = true;
 
   programs.light.enable = true;
-
-  programs.adb.enable = true;
 
   programs.virt-manager.enable = true;
 
