82cd1dcdb6ec9710cfa6f633e504bf477c5d884f
William Hua <william@attente.ca>
2026-06-27 03:04:01 -0400

    nixpkgs-stable

--- flake.nix
+++ flake.nix
@@ -3,15 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hermes-agent.url = "github:NousResearch/hermes-agent/2bd1977d8fad185c9b4be47884f7e87f1add0ce3";
   };
 
-  outputs = { self, nixpkgs, home-manager, hermes-agent, ... }@inputs: {
-    nixosConfigurations.oxygen = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, hermes-agent, ... }@inputs:
+    let
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
+      pkgs-stable = import nixpkgs-stable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+    nixosConfigurations.oxygen = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs pkgs-stable; };
       modules = [
         home-manager.nixosModules.home-manager
         hermes-agent.nixosModules.default
